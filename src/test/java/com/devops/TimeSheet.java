8c6b1590cb43c253cc7fcb3aaf68e03840f7f68f
sudheer <39759708+kumarisk@users.noreply.github.com>
2020-09-04 11:58:17 +0530

    Updated

--- src/test/java/com/devops/TimeSheet.java
+++ src/test/java/com/devops/TimeSheet.java
@@ -17,17 +17,17 @@ public class TimeSheet {
 		System.setProperty("webdriver.gecko.driver", "E:\\automation extenstions\\geckodriver-v0.26.0-win64\\geckodriver.exe");
         driver = new FirefoxDriver();
         
-        driver.get("https://dev.azure.com/Z5X-Tech/Time%20Sheet");
+        driver.get();
         driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 	}
 	
 	@Test
 	public void test() {
-		driver.findElement(By.id("i0116")).sendKeys("sudheer.indlamudi@zee.esselgroup.com");
+		driver.findElement(By.id("i0116")).sendKeys("");
 		
 		driver.findElement(By.id("idSIButton9")).click();
 		
-		driver.findElement(By.id("passwordInput")).sendKeys("Welcome@2020");
+		driver.findElement(By.id("passwordInput")).sendKeys("");
 		
 		driver.findElement(By.id("submitButton")).click();
 		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
